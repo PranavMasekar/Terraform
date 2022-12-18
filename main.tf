d1ccaffd212726af31ccaebd1a678fadf4716499
Pranav <pranavmasekar4@gmail.com>
2022-12-18 13:00:23 +0530

    Terrafor Variables

--- main.tf
+++ main.tf
@@ -1,15 +1,29 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "<YOUR-ACCESS-KEY>"
-  secret_key = "<YOUR-SECRET-KERY>"
+}
+
+variable "subnet-cidr-block" {
+  description = "Subnet cidr block"
+  default = "10.0.10.0/24"
+  type = string
+}
+
+variable "vpc-cidr-block" {
+  description = "VPC vidr block"
+  default = "10.0.0.0/16"
+  type = string
+}
+
+variable "environment" {
+  description = "Deployment Environment"
 }
 
 #! CREATING A VPC IN AWS
 
 resource "aws_vpc" "development-vpc" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = var.vpc-cidr-block
     tags = {
-      Name = "development"
+      Name = var.environment
     }
 }
 
@@ -17,7 +31,7 @@ resource "aws_vpc" "development-vpc" {
 
 resource "aws_subnet" "devlopment-subnet-1" {
   vpc_id = aws_vpc.development-vpc.id
-  cidr_block = "10.0.10.0/24"
+  cidr_block = var.subnet-cidr-block
   availability_zone = "ap-south-1a"
    tags = {
       Name = "subnet-1-dev"
@@ -26,17 +40,27 @@ resource "aws_subnet" "devlopment-subnet-1" {
 
 #! GETTING EXISTING RESOURCE IN AWS
 
-data "aws_vpc" "existing-vpc" {
-  default = true
-}
+# data "aws_vpc" "existing-vpc" {
+#   default = true
+# }
 
 #! CREATING SUBNET IN EXISTING VPC
 
-resource "aws_subnet" "devlopment-subnet-2" {
-  vpc_id = data.aws_vpc.existing-vpc.id
-  cidr_block = "172.31.48.0/20"
-  availability_zone = "ap-south-1a"
-   tags = {
-      Name = "subnet-2-default"
-    }
+# resource "aws_subnet" "devlopment-subnet-2" {
+#   vpc_id = data.aws_vpc.existing-vpc.id
+#   cidr_block = "172.31.48.0/20"
+#   availability_zone = "ap-south-1a"
+#    tags = {
+#       Name = "subnet-2-default"
+#     }
+# }\
+
+#! OUTPUT VARIABLES AFTER TERRAFORM APPLY
+
+output "dev-vpc-id" {
+  value = aws_vpc.development-vpc.id
+}
+
+output "dev-subnet-id" {
+  value = aws_subnet.devlopment-subnet-1.id
 }
---
+++ terraform-dev.tfvars
@@ -0,0 +1,4 @@
+subnet-cidr-block = "10.0.30.0/24"
+vpc-cidr-block = "10.0.0.0/16"
+
+environment = "development"
