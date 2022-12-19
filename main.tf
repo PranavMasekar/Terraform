4f50dcb03604af65ba4aae51b0b82160c757cbcb
Pranav <pranavmasekar4@gmail.com>
2022-12-19 13:09:07 +0530

    Created VPC,SUBNET,INTERNET GATEWAY,ROUTE TABLE,ROUTE TABLE ASSOCIATION

--- main.tf
+++ main.tf
@@ -2,65 +2,46 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-variable "subnet-cidr-block" {
-  description = "Subnet cidr block"
-  default = "10.0.10.0/24"
-  type = string
-}
+variable "vpc-cidr-block" {}
+variable "subnet-cidr-block" {}
+variable "availability_zone" {}
+variable "env_prefix" {}
 
-variable "vpc-cidr-block" {
-  description = "VPC vidr block"
-  default = "10.0.0.0/16"
-  type = string
-}
-
-variable "environment" {
-  description = "Deployment Environment"
-}
-
-#! CREATING A VPC IN AWS
-
-resource "aws_vpc" "development-vpc" {
+resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc-cidr-block
     tags = {
-      Name = var.environment
+      Name = "${var.env_prefix}-vpc"
     }
 }
 
-#! CREATING SUBNET IN ABOVE VPC
-
-resource "aws_subnet" "devlopment-subnet-1" {
-  vpc_id = aws_vpc.development-vpc.id
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
   cidr_block = var.subnet-cidr-block
-  availability_zone = "ap-south-1a"
+  availability_zone = var.availability_zone
    tags = {
-      Name = "subnet-1-dev"
+      Name = "${var.env_prefix}-subnet-1"
     }
 }
 
-#! GETTING EXISTING RESOURCE IN AWS
-
-# data "aws_vpc" "existing-vpc" {
-#   default = true
-# }
-
-#! CREATING SUBNET IN EXISTING VPC
-
-# resource "aws_subnet" "devlopment-subnet-2" {
-#   vpc_id = data.aws_vpc.existing-vpc.id
-#   cidr_block = "172.31.48.0/20"
-#   availability_zone = "ap-south-1a"
-#    tags = {
-#       Name = "subnet-2-default"
-#     }
-# }\
-
-#! OUTPUT VARIABLES AFTER TERRAFORM APPLY
-
-output "dev-vpc-id" {
-  value = aws_vpc.development-vpc.id
+resource "aws_internet_gateway" "myapp-internet-gateway" {
+  vpc_id = aws_vpc.myapp-vpc.id
+   tags = {
+    Name = "${var.env_prefix}-internet-gateway"
+  }
 }
 
-output "dev-subnet-id" {
-  value = aws_subnet.devlopment-subnet-1.id
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-internet-gateway.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-route-table"
+  }
+}
+
+resource "aws_route_table_association" "myapp-association" {
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
 }
