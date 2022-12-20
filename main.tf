44bdf0a6337171db2dbc832af8f3f61d61ae4931
Pranav <pranavmasekar4@gmail.com>
2022-12-20 15:48:23 +0530

    DEPLOYED OWN DOCKER IMAGE

--- main.tf
+++ main.tf
@@ -56,8 +56,8 @@ resource "aws_security_group" "myapp-security-group" {
   }
 
   ingress  {
-    from_port = 8080
-    to_port = 8080
+    from_port = 8000
+    to_port = 8000
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
