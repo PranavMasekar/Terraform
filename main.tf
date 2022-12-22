163ea8d307d0e1db9bb8f184a9e3800b9e2eeb81
Pranav <pranavmasekar4@gmail.com>
2022-12-22 10:44:11 +0530

    USED PREDEFINED MODULES

--- main.tf
+++ main.tf
@@ -1,27 +1,29 @@
-resource "aws_vpc" "myapp-vpc" {
-  cidr_block = var.vpc-cidr-block
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = var.vpc-cidr-block
+
+  azs             = [var.availability_zone]
+  public_subnets  = [var.subnet-cidr-block]
+
   tags = {
     Name = "${var.env_prefix}-vpc"
   }
-}
 
-module "my-app-subnet" {
-  source                 = "./modules/subnet"
-  subnet-cidr-block      = var.subnet-cidr-block
-  availability_zone      = var.availability_zone
-  env_prefix             = var.env_prefix
-  vpc_id                 = aws_vpc.myapp-vpc.id
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  public_subnet_tags = {
+    Name = "${var.env_prefix}-subnet-1"
+  }
 }
 
 module "myapp-server" {
   source            = "./modules/server"
-  vpc_id            = aws_vpc.myapp-vpc.id
+  vpc_id            = module.vpc.vpc_id
   ip-address        = var.ip-address
   env_prefix        = var.env_prefix
   image_name        = var.image_name
   public-key-path   = var.public-key-path
   instance-type     = var.instance-type
-  subnet_id         = module.my-app-subnet.subnet.id
+  subnet_id         = module.vpc.public_subnets[0]
   availability_zone = var.availability_zone
 }
--- modules/subnet/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "subnet-cidr-block" {}
-variable "availability_zone" {}
-variable "env_prefix" {}
-variable "vpc_id" {}
-variable "default_route_table_id" {}
